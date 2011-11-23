c7be6d43598f487a21090c5bf557cbdaa0012338
Andreas Møller <andreas.moller@gmail.com>
2011-11-23 17:24:27 +0100

    commit

--- src/SmartHouse/SmartHouse.java
+++ src/SmartHouse/SmartHouse.java
@@ -1,12 +1,53 @@
 package SmartHouse;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class SmartHouse{
+    Connection conn = null;
+
     public SmartHouse(){
+                try {
+            Class.forName("com.mysql.jdbc.Driver");//load the mysql driver
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/kiiib?user=KIIIB&password=42");//connect to the database
+        }
+        catch (SQLException se){
+            System.out.println("SQLException: " + se.getMessage());
+            System.out.println("SQLState: " + se.getSQLState());
+            System.out.println("VendorError: " + se.getErrorCode());
+ 
+        }
+        catch (Exception e){
+             e.printStackTrace();
+       }
         System.out.println("HEllo SCALA!!");
     }
     public void sensorEvent(int sensorId){
-        
+        try{
+            System.out.println("Sensor "+sensorId+" fired!");        
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO sensor_events VALUES("+sensorId+",NOW())");
+        }
+        catch(SQLException se){
+            System.out.println("SQLException: " + se.getMessage());
+            System.out.println("SQLState: " + se.getSQLState());
+            System.out.println("VendorError: " + se.getErrorCode());
+        }
     }
     public void switchEvent(int switchId, int status){
+        try{
+            System.out.println("Switch "+switchId+" turned "+status);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("INSERT INTO switch_events VALUES("+switchId+","+status+",NOW())");
+
+    
+        }
+        catch(SQLException se){
+            System.out.println("SQLException: " + se.getMessage());
+            System.out.println("SQLState: " + se.getSQLState());
+            System.out.println("VendorError: " + se.getErrorCode());
+        }
     }
 
 }
