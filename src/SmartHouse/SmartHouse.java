cdb30611bfeff8d2e2c376adb7c0906d0de4f99d
Andreas Møller <andreas.moller@gmail.com>
2011-11-23 16:27:17 +0100

    first commit

---
+++ src/SmartHouse/SmartHouse.java
@@ -0,0 +1,12 @@
+package SmartHouse;
+public class SmartHouse{
+    public SmartHouse(){
+        System.out.println("HEllo SCALA!!");
+    }
+    public void sensorEvent(int sensorId){
+        
+    }
+    public void switchEvent(int switchId, int status){
+    }
+
+}
