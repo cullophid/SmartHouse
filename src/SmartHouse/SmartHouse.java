622cdfd8a929763135a435b333c379f588c28a6d
Andreas Møller <andreas.moller@gmail.com>
2011-12-02 15:00:32 +0100

    added .jar to gitignore

--- src/SmartHouse/SmartHouse.java
+++ src/SmartHouse/SmartHouse.java
@@ -1,18 +1,33 @@
 package SmartHouse;
+//hopefully non conflicting test commit
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SmartHouse{
+	
     Connection conn = null;
     Statement stmt;
+    
+    public static void main(String[] args) {        
+        Map<Integer, Boolean> lamps = new HashMap<Integer, Boolean>();
+        lamps.put(3, false);
+        lamps.put(7, false);
+        lamps.put(42, true);
+        for(int key : lamps.keySet()) {
+            System.out.println(key + "-->" + lamps.get(key));
+        }
+    }
     /*
      * Constructor for the class SmartHouse
      * Handles the input and output for the ai
      */
     public SmartHouse(){
-                try {
+        try {
             Class.forName("com.mysql.jdbc.Driver");//load the mysql driver
             conn = DriverManager.getConnection("jdbc:mysql://localhost/kiiib?user=KIIIB&password=42");//connect to the database
             stmt = conn.createStatement();
@@ -28,7 +43,7 @@ public class SmartHouse{
         catch (Exception e){
              e.printStackTrace();
        }
-        System.out.println("HEllo SCALA!!");
+       System.out.println("Hello SCALA!!");
     }
     /*
      * Method called when a sensorevent occurs in the simulator
@@ -59,5 +74,15 @@ public class SmartHouse{
             System.out.println("SQLState: " + se.getSQLState());
             System.out.println("VendorError: " + se.getErrorCode());
         }
+    }
+    
+    public Map<Integer, Boolean> shouldLampsBeTurnedOn(int sensorId) {
+        Map<Integer, Boolean> lamps = new HashMap<Integer, Boolean>();
+        lamps.put(sensorId, true);
+        lamps.put(3, false);
+        lamps.put(7, false);
+        lamps.put(42, true);
+        
+        return lamps;
     }
 }
