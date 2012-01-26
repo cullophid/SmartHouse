3c05a9edef327663f108acc60ca26e55d81c5a4e
Andreas Møller <andreas.moller@gmail.com>
2012-01-26 15:50:19 +0100

    tried(emphasis on tried) to fix the conflicts

--- src/events/EventList.java
+++ src/events/EventList.java
@@ -4,28 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import java.util.Iterator;
 import java.util.LinkedList;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-import java.util.Iterator;
-import java.util.LinkedList;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-import java.util.Iterator;
-import java.util.LinkedList;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-import java.util.Iterator;
-import java.util.LinkedList;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
 import java.util.List;
-
 import config.Config;
 
 public class EventList {
@@ -42,28 +23,8 @@ public class EventList {
     /**
      * Time interval stored in the event list. 
      */
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private int pattern_interval = 10*1000;
+    private int pattern_interval =  10*1000;
     private int pattern_length = 7;
-=======
-    private int pattern_interval =  10*1000;
-    private int pattern_depth = 7;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-    private int pattern_interval =  10*1000;
-    private int pattern_depth = 7;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-    private int pattern_interval =  10*1000;
-    private int pattern_depth = 7;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-    private int pattern_interval =  10*1000;
-    private int pattern_depth = 7;
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
     
     public static void main(String[] args) {
         EventList list = new EventList();
@@ -74,28 +35,8 @@ public class EventList {
     }
     
     public EventList() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        events = new ArrayList<Event>(pattern_length);
-        zone = new ArrayList<Event>(3);
-=======
         events = new LinkedList<Event>();
         zone = new LinkedList<Event>();
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-        events = new LinkedList<Event>();
-        zone = new LinkedList<Event>();
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-        events = new LinkedList<Event>();
-        zone = new LinkedList<Event>();
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
-        events = new LinkedList<Event>();
-        zone = new LinkedList<Event>();
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
     }
     
     public EventList(int zone_interval, int pattern_interval) {
@@ -124,10 +65,6 @@ public class EventList {
         if(events.size() > 0 && System.currentTimeMillis() - events.get(events.size()-1).getTS() > pattern_interval)
             events.clear();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         if (events.size() >= pattern_length)
             events.remove(0);
     }
@@ -144,13 +81,6 @@ public class EventList {
         boolean duplicates = false;
         for (int i = zone.size() - 1; i >= 0; i--) {
             if (zone.get(i).compareID(e.getID())) {
-=======
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
         if (events.size() >= pattern_depth)
             events.remove(0);
     }
@@ -175,16 +105,6 @@ public class EventList {
         boolean duplicates = false;
         for (int i = zone.size() - 1; i >= 0; i--) {
             if (zone.get(i).getID() == e.getID()) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
                 duplicates = true;
             }
                         
@@ -192,31 +112,10 @@ public class EventList {
                 zone.remove(i);
         }
         zone.add(e);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
 
         //limit to 4 sensors
         while (zone.size() > 4)
             zone.remove(0);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
         
         if(zone.size() >= 2) {
             events.add(new ZoneEvent(zone));
@@ -240,10 +139,6 @@ public class EventList {
         add(new SwitchEvent(id, cmd));
     }
     
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     //TODO various get methods
     public Event[] getEvents() {
         Event[] array = new Event[events.size()];
@@ -258,13 +153,6 @@ public class EventList {
         return array;
     }
     
-=======
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
     /**
      * get events in event list, including detected zone events
      * @return
@@ -291,14 +179,4 @@ public class EventList {
         }
         return pattern;
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
-=======
->>>>>>> fe34afac81b863f1ed4f4a02b278a4c13ff5b7f2
 }
